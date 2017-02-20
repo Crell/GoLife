8dd95e52615c08b32fdfacd2f9e3465eb5ead452
Larry Garfield <larry@garfieldtech.com>
2017-02-20 16:57:14 -0600

    Add tests for isPlayer()

--- golife_test.go
+++ golife_test.go
@@ -27,10 +27,28 @@ func TestCellUpdateValue(t *testing.T) {
 		c.updateValue()
 
 		if tt.expected != c.state {
-			t.Error("Expected %s, got %s", tt.expected, c.state)
+			t.Errorf("Expected %s, got %s", tt.expected, c.state)
 		}
+	}
+}
 
+func TestPlayerDetection(t *testing.T) {
+
+	tests := []struct{
+		state cellState
+		expected bool
+	} {
+		{cellEmpty, false},
+		{cellFood, false},
+		{cellRock, false},
+		{cellState("1"), true},
+		{cellState("0"), true},
 	}
 
-
+	for _, tt := range(tests) {
+		c := &cell{state: tt.state}
+		if c.isPlayer() != tt.expected {
+			t.Errorf("State %s: Got %s, expected %s", tt.state, c.isPlayer(), tt.expected)
+		}
+	}
 }
